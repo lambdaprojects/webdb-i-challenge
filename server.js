f53d59ee3ff29b919351d0f0c01ff7505cdf84b1
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-03 14:29:07 -0500

    Initial files. yarn upgrade, yarn express, index.js, server.js and the server is running in port 4000

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
+server.use(express.json());
 
 // your code here
+server.get("/", (req, res) => {
+  res.send(`<h2>Ready, set go! for webapidb Challenge</h2>`);
+});
 
 module.exports = server;
