6c9c6d55d5cd9db161ae7d4a6d4c2ba047fb5882
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-03 14:49:58 -0500

    Completed findById. Inserted two rows into the db and retrieved one of them at a time

--- data/accountsRouter.js
+++ data/accountsRouter.js
@@ -14,11 +14,50 @@ router.get("/", async (req, res) => {
         .json({ ERROR_MESSAGE: "There are no accounts to display" });
     }
   } catch (error) {
-    console.log(error);
     res.status(500).json({
       ERROR_MESSAGE: "There was an error while retrieving the accounts."
     });
   }
 });
 
+// GET PROJECT FOR THE ID PASSED AS PARAM
+router.get("/:id", validateAccountsId, async (req, res) => {
+  try {
+    res.status(200).json(req.account);
+  } catch (error) {
+    res.status(500).json({
+      ERROR_MESSAGE: "There was an error while retrieving the accounts."
+    });
+  }
+});
+
+// This is a custom middleware function to validate accounts Id
+// The following validations have been performed.
+// 1. Check if the id exist in the req params.
+// 2. Check if id is not null 0 or empty string
+// 3. Check if the id is available in the database
+async function validateAccountsId(req, res, next) {
+  if (req.params.id) {
+    if (req.params.id !== 0 && req.params.id !== null && req.params.id !== "") {
+      const account = await accountsHelper.findById(req.params.id);
+      if (account) {
+        req.account = account;
+        next();
+      } else {
+        res.status(400).json({
+          ERROR_MESSAGE:
+            "No account available for this post id in the database."
+        });
+      }
+    } else {
+      res.status(400).json({
+        ERROR_MESSAGE: "The account id provided is either null or empty."
+      });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ ERROR_MESSAGE: "There is no account id available." });
+  }
+}
 module.exports = router;
